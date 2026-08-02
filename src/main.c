c1cb561af3033dc27844b84a2e5238bc30451762
Xwilarg <xwilarg@protonmail.com>
2026-08-02 12:57:21 +0200

    Add scrolling example

--- src/main.c
+++ src/main.c
@@ -1,10 +1,23 @@
 #include <snes.h>
 
+extern char patterns, patterns_end;
+extern char palette;
+extern char map, map_end;
+
+
 int main(void)
 {
+    u16 scrX = 0, scrY = 0;
+    u16 pad0, move;
+
     // Initialize text console with our font
     // Default Map is 0x6800, Gfx is 0x3000 and offset is 0
     consoleInitDefaultText(0);
+    
+    // Copy tiles to VRAM
+    bgInitTileSet(1, &patterns, &palette, 0, (&patterns_end - &patterns), 16 * 2, BG_16COLORS, 0x4000);
+    // Copy Map to VRAM
+    bgInitMapSet(1, &map, (&map_end - &map), SC_64x64, 0x1000);
 
     // Init background
     bgSetGfxPtr(0, 0x3000);
@@ -12,19 +25,46 @@ int main(void)
 
     // Now Put in 16 color mode and disable Bgs except current
     setMode(BG_MODE1, 0);
-    bgSetDisable(1);
     bgSetDisable(2);
-
-    // Draw a wonderful text :P
-    consoleDrawText(10, 10, "Hello World!");
-    consoleDrawText(6, 14, "WELCOME TO PVSNESLIB");
-    consoleDrawText(3, 18, "HTTPS://WWW.PORTABLEDEV.COM");
 
     // Wait for nothing :P
     setScreenOn();
 
+    // Wait for nothing :P
     while (1)
     {
+        // no move currently
+        move = 0;
+
+        // Get current #0 pad
+        pad0 = padsCurrent(0);
+
+        // Update scrolling with current pad
+        switch (pad0)
+        {
+        case KEY_RIGHT:
+            scrX += 2;
+            move = 1;
+            break;
+        case KEY_LEFT:
+            scrX -= 2;
+            move = 1;
+            break;
+        case KEY_DOWN:
+            scrY += 2;
+            move = 1;
+            break;
+        case KEY_UP:
+            scrY -= 2;
+            move = 1;
+            break;
+        }
+        if (move)
+            bgSetScroll(1, scrX, scrY);
+
+        // Just inform user
+        consoleDrawText(0, 0, "SCR X=%d Y=%d", scrX, scrY);
+
         WaitForVBlank();
     }
     return 0;
