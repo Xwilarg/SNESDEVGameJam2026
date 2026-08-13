b91ba01c79c1d2361ebc8b03c2eea81d1940ec01
Xwilarg <xwilarg@protonmail.com>
2026-08-13 16:09:34 +0200

    Start adding report

--- src/report.c
+++ src/report.c
@@ -1,1 +1,17 @@
 #include "report.h"
+
+static u16 pad0;
+
+
+static void ReachCountry(void)
+{
+}
+
+static void Update(bool isTurnStarted)
+{
+}
+
+Phase* Report_GetPhase()
+{
+    return NULL;
+}
--- src/report.h
+++ src/report.h
@@ -1,4 +1,8 @@
 #ifndef REPORT_H
 #define REPORT_H
 
+#include "phase.h"
+
+Phase* Report_GetPhase();
+
 #endif
