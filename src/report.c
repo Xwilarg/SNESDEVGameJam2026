ec8558e3b001aeaa503d992361eca4706ac5d966
Xwilarg <xwilarg@protonmail.com>
2026-08-13 16:10:08 +0200

    Add report empty template

--- src/report.c
+++ src/report.c
@@ -2,6 +2,13 @@
 
 static u16 pad0;
 
+static void ReachCountry(void);
+static void Update(bool isTurnStarted);
+
+static Phase phase = {
+    &ReachCountry,
+    &Update
+};
 
 static void ReachCountry(void)
 {
@@ -9,9 +16,14 @@ static void ReachCountry(void)
 
 static void Update(bool isTurnStarted)
 {
+    pad0 = padsDown(0);
+
+    switch (pad0)
+    {
+    }
 }
 
 Phase* Report_GetPhase()
 {
-    return NULL;
+    return &phase;
 }
